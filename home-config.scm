504ce5bc8bea63d5dc64236fb1e40b2af18b6f84
nomike <nomike@nomike.cpm>
2025-03-28 04:14:12 +0100

    Install OpenSCAD.

--- home-config.scm
+++ home-config.scm
@@ -1,12 +1,13 @@
 (define-module (home-config)
   #:use-module (gnu home)
+  #:use-module (gnu packages engineering)
   #:use-module (gnu packages version-control)
   #:use-module (gnu services)
-  #:use-module (emacs-packages)); This imports the emp module
+  #:use-module (emacs-packages))
 
 (home-environment
   (packages (append
       emacs-packages ; Note: just 'emp', not '(emp)' since it's a list, not a function
       (list
-        ;; You can add other packages here
+        openscad
         ))))
