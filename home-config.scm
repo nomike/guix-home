c06248435a91d82b6cf9c74df6f8998256e8a60d
nomike <nomike@nomike.cpm>
2025-03-31 21:04:38 +0200

    Use one big specifications->packages instead of several small specification->package.

--- home-config.scm
+++ home-config.scm
@@ -1,13 +1,13 @@
 (define-module (home-config)
   #:use-module (gnu home)
-  #:use-module (gnu packages engineering)
-  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages)
   #:use-module (gnu services)
   #:use-module (emacs-packages))
 
 (home-environment
-  (packages (append
-      emacs-packages ; Note: just 'emp', not '(emp)' since it's a list, not a function
-      (list
-        openscad
-        ))))
+  (packages
+    (append
+        emacs-packages
+    )
+  )
+)
