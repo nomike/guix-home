348875a90bcea852ef33236a8a71743db6cede14
nomike <nomike@nomike.com>
2025-05-09 05:09:58 +0200

    Add developer-workstation profile module with initial package structure

---
+++ profiles/developer-workstation.scm
@@ -0,0 +1,13 @@
+(define-module (profiles developer-workstation)
+  ; #:use-module (modules basic-shell-tools)
+  ; #:use-module (modules emacs)
+  ; #:use-module (modules programming)
+  #:export (developer-workstation-packages))
+
+(define-public developer-workstation-packages
+  (append
+  ;  basic-shell-tools-packages
+  ;  emacs-packages
+  ;  programming-packages
+   )
+  )
