3233d2741fd63682bc9ed4b156142756cd0ba02d
nomike <nomike@nomike.com>
2025-05-09 05:09:19 +0200

    Add programming module with tree-sitter grammars for various languages

---
+++ modules/programming.scm
@@ -0,0 +1,18 @@
+(define-module (modules programming)
+  #:use-module (gnu home)
+  #:use-module (gnu packages)
+  #:use-module (gnu services)
+  #:use-module (guix transformations)
+  #:export (programming-packages)
+  )
+
+(define-public programming-packages
+  (append 
+   (specifications->packages
+    (list
+     "tree-sitter-scheme" ; tree-sitter Scheme (R5RS, R6RS) grammar
+     "tree-sitter-python" ; Tree-sitter Python grammar
+     "tree-sitter-bash" ; Tree-sitter Bash grammar
+     "tree-sitter-org" ; Tree-sitter Org grammar
+     ))
+   ))
