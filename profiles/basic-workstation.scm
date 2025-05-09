fb6998d1fc9bb9ef2785c2d4c5b0a396bff59311
nomike <nomike@nomike.com>
2025-05-09 05:09:42 +0200

    Reorder module imports and package list in basic-workstation profile

--- profiles/basic-workstation.scm
+++ profiles/basic-workstation.scm
@@ -1,11 +1,13 @@
 (define-module (profiles basic-workstation)
-  #:use-module (modules emacs)
   #:use-module (modules basic-shell-tools)
+  #:use-module (modules emacs)
+  #:use-module (modules programming) ;; TODO: Move to developer-workstation once the issue is fixed
   #:export (basic-workstation-packages))
 
 (define-public basic-workstation-packages
   (append
-    emacs-packages
-    basic-shell-tools-packages
+   basic-shell-tools-packages
+   emacs-packages
+   progamming ;; TODO: Move to developer-workstation once the issue is fixed
+   )
   )
-)
