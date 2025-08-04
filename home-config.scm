b439965274f552bc5f3c674446a29e3c9c701b96
nomike <nomike@nomike.com>
2025-08-04 12:11:53 +0200

    Remove pythonscad package as it has been merged to guix.

--- home-config.scm
+++ home-config.scm
@@ -1,17 +1,12 @@
 (define-module (home-config)
   #:use-module (gnu home)
   #:use-module (gnu packages base)
-  #:use-module (packages pythonscad)
-  #:use-module (roles scrooge)
-  )
+  #:use-module (roles scrooge))
 
 
 (home-environment
- (packages
-  (append
-   scrooge-packages
-   (list
-   pythonscad)
-   )
-  )
- )
+  (packages
+   (append
+    scrooge-packages
+    (list
+     ))))
