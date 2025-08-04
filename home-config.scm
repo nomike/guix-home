4a94b91c7c5e93b9db85e9159b73c6fb3abed65d
nomike <nomike@nomike.com>
2025-08-04 12:08:03 +0200

    Remove custom package for openscad as it hs been merged to guix

--- home-config.scm
+++ home-config.scm
@@ -1,7 +1,6 @@
 (define-module (home-config)
   #:use-module (gnu home)
   #:use-module (gnu packages base)
-  #:use-module (packages openscad)
   #:use-module (packages pythonscad)
   #:use-module (roles scrooge)
   )
@@ -12,7 +11,6 @@
   (append
    scrooge-packages
    (list
-  ;  openscad
    pythonscad)
    )
   )
