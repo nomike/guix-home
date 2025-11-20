04c8e35ea4187fc12ba9d3102890112823eb9d95
nomike Postmann <nomike.postmann@paysafe.com>
2025-11-20 11:57:10 +0100

    Install just

--- modules/programming.scm
+++ modules/programming.scm
@@ -5,15 +5,16 @@
   #:use-module (guix transformations))
 
 (define-public programming-packages
-  (append 
+  (append
    (specifications->packages
     (list
      "cppcheck" ; "Static C/C++ code analyzer"
+     "cloc" ; Count source lines of code (SLOC) and other source code metrics
+     "just" ; Command runner
      "tree-sitter-scheme" ; tree-sitter Scheme (R5RS, R6RS) grammar
      "tree-sitter-python" ; Tree-sitter Python grammar
      "tree-sitter-bash" ; Tree-sitter Bash grammar
      "tree-sitter-org" ; Tree-sitter Org grammar
      "uncrustify" ; Code formatter for C and other related languages
      "valgrind" ; Debugging and profiling tool suite
-     "cloc" ; Count source lines of code (SLOC) and other source code metrics
      ))))
